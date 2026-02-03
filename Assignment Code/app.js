4c34e5c4ead09a27ab5cdb1abab18ca87850b206
Mohammad Ameen <mohammad.ameen@powerhouse.ca>
2026-02-03 00:40:32 -0600

    map elements in HTML to Javascript

---
+++ Assignment Code/app.js
@@ -0,0 +1,16 @@
+let title=document.getElementById("title");
+let lat=document.getElementById("latitude");
+let lon=document.getElementById("longitude");
+let img=document.getElementById("img");
+let landmarkList=document.getElementById("landmarkList");
+let details=document.getElementById("details");
+let useLocationBtn=document.getElementById("useLocation");
+
+
+console.log(img);
+console.log(lat);
+console.log(lon);
+console.log(title);
+console.log(landmarkList);
+console.log(details);
+console.log(useLocationBtn);
