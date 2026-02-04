29c3b79334f7a3ea64de5506fcf28874d461f6f9
Mohammad Ameen <mohammad.ameen@powerhouse.ca>
2026-02-03 23:27:39 -0600

    Added event listeners for all form information, used W3 schools under Event listeners for Resources

--- Assignment Code/app.js
+++ Assignment Code/app.js
@@ -4,13 +4,38 @@ let lon=document.getElementById("longitude");
 let img=document.getElementById("img");
 let landmarkList=document.getElementById("landmarkList");
 let details=document.getElementById("details");
+let description=document.getElementById("description");
 let useLocationBtn=document.getElementById("useLocation");
-
+let landmarkform=document.getElementById("landmarkform");
 
 console.log(img);
 console.log(lat);
 console.log(lon);
 console.log(title);
 console.log(landmarkList);
+console.log(description);
 console.log(details);
 console.log(useLocationBtn);
+console.log(landmarkform);
+
+landmarkform.addEventListener("submit",function(event){
+    event.preventDefault();
+    console.log("Form Submitted");
+
+    titleValue=title.value;
+    console.log(titleValue);
+
+    latValue=lat.value;
+    console.log(latValue);
+
+    lonValue=lon.value;
+    console.log(lonValue);
+    
+    imgValue=img.value;
+    console.log(imgValue);
+    
+    descriptionValue=description.value;
+    console.log(descriptionValue);
+    
+});
+
