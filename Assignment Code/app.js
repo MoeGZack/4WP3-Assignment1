e81d0ea4c9cf6007ad74d207d6232018e9d6895e
Mohammad Ameen <mohammad.ameen@powerhouse.ca>
2026-02-05 01:00:09 -0600

    Added markers, and details pull up when selected. It will show on details page. Also geolocation will showcase on page load

--- Assignment Code/app.js
+++ Assignment Code/app.js
@@ -12,27 +12,46 @@ let selectedLandmark=null;
 let landmarkdeleted=null;
 const landmarks=[];
 let map;
-window.initMap = initMap;
 
 
-async function initMap() {
-    {
-      
-        map = new google.maps.Map(document.getElementById("map"), 
-        
-          {
-            center: {lat: 43.2375, lng: -79.8341},
-            zoom: 12,
-            mapId: "89d95e771da5be8b5be36e75"
-          });
+function handleFindMe(){
+    
+      navigator.geolocation.getCurrentPosition( showPosition);
     }
-}
+   window.addEventListener("load",handleFindMe);
+    document.getElementById("useLocation").addEventListener("click",handleFindMe);
+
+
+
+function showPosition(position){
+    
+      lat.value=position.coords.latitude;
+      lon.value=position.coords.longitude;
+    
+
+  const latitudeStart=parseFloat(lat.value);
+  const lonStart=parseFloat(lon.value);
+  
+if (!map){
+    
+        map = new google.maps.Map(document.getElementById("map"), {
+                center: {lat: latitudeStart, lng: lonStart},
+            zoom: 12,
+            mapId: "89d95e771da5be8b5be36e75",
+            clickableIcons: false
+          });
+    } else {
+      map.setCenter({lat: latitudeStart, lng: lonStart});
+      map.setZoom(12);
+    }
+
+  }
 
 function renderLandmarkList() {
     landmarkList.innerHTML = "";
-   landmarks.forEach(myFunction);
+   landmarks.forEach(appendArray);
 
-function myFunction(item) {
+function appendArray(item) {
 
 const node = document.createElement("li");
 
@@ -52,7 +71,10 @@ node.addEventListener("click", function() {
   "Latitude: " + item.latitude + "<br>" +
   "Longitude: " + item.longitude + "<br>" +
   "Description: " + item.description + "<br>" +
-  "<img src='" + item.imageUrl + "' width='200' height='200'><br>"
+  "<img src='" + item.imageUrl + "' width='200' height='200'><br>";
+
+  map.setCenter({lat: item.latitude, lng: item.longitude});
+    map.setZoom(14);
 });
 }
 
@@ -79,15 +101,35 @@ landmarkform.addEventListener("submit",function(event){
   imageUrl: imgValue,
   description: descriptionValue
 };
+
 landmarks.push(Landmark);
 console.log("landmarks", landmarks);
 
+const marker= new google.maps.Marker({
+position: {lat: latValue, lng: lonValue},
+map: map,
+title: titleValue,
+zIndex: 100000
+});
 
-map.setCenter({lat: latValue, lng: lonValue});
-map.setZoom(14);
+Landmark.marker=marker;
+
+marker.addListener("click", function() {
+  selectedLandmark=Landmark.id;
+  landmarkdeleted=Landmark.title;
+
+
+details.innerHTML = "Landmark ID:" +Landmark.id + "<br>" +
+  "Location:" + Landmark.title + "<br>" +
+  "Latitude: " + Landmark.latitude + "<br>" +
+  "Longitude: " + Landmark.longitude + "<br>" +
+  "Description: " + Landmark.description + "<br>" +
+  "<img src='" + Landmark.imageUrl + "' width='200' height='200'><br>";
+map.setCenter({lat: Landmark.latitude, lng: Landmark.longitude});
+    map.setZoom(14);
+});
+
 renderLandmarkList();
-
-
 landmarkform.reset();
   });
 reader.readAsDataURL(img.files[0]);
@@ -96,6 +138,10 @@ deleteLandmark.addEventListener("click", function() {
 
     console.log("Deleting landmark ID:", selectedLandmark, "Title:", landmarkdeleted);
     const newlandmarks = landmarks.filter(deletelandmark);
+    const lm = landmarks.find(l => l.id === selectedLandmark);
+    if (lm && lm.marker) {
+        lm.marker.setMap(null);
+    }
      function deletelandmark(landmark) {
         return landmark.id !== selectedLandmark;
     }
